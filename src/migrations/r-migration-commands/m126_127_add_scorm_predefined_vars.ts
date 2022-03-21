4bfa0b845b69c42330355ac278068e6c34957284
pokerverse <siddharth.pokharna@gmetri.com>
2022-03-21 10:51:56 +0000

    [fix] comment

--- src/migrations/r-migration-commands/m126_127_add_scorm_predefined_vars.ts
+++ src/migrations/r-migration-commands/m126_127_add_scorm_predefined_vars.ts
@@ -9,7 +9,6 @@ class Migration implements IOrder {
     // ! Add all predefined vars to this project, below function internally handles in cases of conflicts
     projectF.addPredefinedVariable(vn.PredefinedVariableName.scorm_progress);
     projectF.addPredefinedVariable(vn.PredefinedVariableName.scorm_suspend_data);
-    projectF.addPredefinedVariable(vn.PredefinedVariableName.scorm_score);
 
     projectF.set(rtp.project.version, 127);
   }
