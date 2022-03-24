639629294bfa0fc4239946ec124d7704a79f5aba
pokerverse <siddharth.pokharna@gmetri.com>
2022-03-24 07:34:02 +0000

    [add] linked variables for embed scorm element

--- src/r/definitions/elements/types/EmbedSCORM.ts
+++ src/r/definitions/elements/types/EmbedSCORM.ts
@@ -8,7 +8,10 @@ export const EmbedScorm: IElementDefinition = {
   elementDefaultName: "SCORM",
   properties: [
     ...BasicElement.properties,
-    ElementProperty.source
+    ElementProperty.source,
+    ElementProperty.embed_scorm_score_var_id,
+    ElementProperty.embed_scorm_suspend_data_var_id,
+    ElementProperty.embed_scorm_progress_var_id,
   ],
   defaultOverrides: {},
   events: [ RuleEvent.on_scorm_finish, RuleEvent.on_scorm_initialize, RuleEvent.on_scorm_set_score, RuleEvent.on_close],
