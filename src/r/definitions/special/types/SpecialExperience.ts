3ff66b5c9b9bcb35a49c4438cf1cb1508189b162
Siddharth <siddharth.pokharna@gmetri.com>
2022-07-12 15:12:30 +0530

    [add] new then action called open deployment

--- src/r/definitions/special/types/SpecialExperience.ts
+++ src/r/definitions/special/types/SpecialExperience.ts
@@ -8,6 +8,7 @@ export const SpecialExperience: ISpecialDefinition = {
   actions: [
     RuleAction.change_scene,
     RuleAction.open_url,
+    RuleAction.open_deployment,
     RuleAction.copy_to_clipboard,
     RuleAction.end_experience,
     RuleAction.take_screenshot,
