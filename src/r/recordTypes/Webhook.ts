cd62e5e1c5b142683dd2f25ff277aa743d3baa44
Sahil Ahuja <sahilahuja@gmail.com>
2022-09-13 11:11:05 +0000

    Auth creds

--- src/r/recordTypes/Webhook.ts
+++ src/r/recordTypes/Webhook.ts
@@ -3,6 +3,9 @@ export enum WebhookProperty {
   webhook_type = "webhook_type", //rules | session | variables | legacy (for the older version of webhooks)
   webhook_auth_type = "webhook_auth_type", //none, basic, jwt
   webhook_api_endpoint = "webhook_api_endpoint",
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Authorization
+  webhook_auth_basic_credentials = "webhook_auth_basic_credentials",
+  webhook_auth_jwt_secret = "webhook_auth_jwt_secret",
 }
 
 export const webhookPropertyDefaults:  Record<WebhookProperty, unknown> = {
@@ -10,4 +13,6 @@ export const webhookPropertyDefaults:  Record<WebhookProperty, unknown> = {
   [WebhookProperty.webhook_type]: "variables", //Definitions.WebhookType.variables
   [WebhookProperty.webhook_auth_type]: "none", //Definitions.WebhookAuthType.none
   [WebhookProperty.webhook_api_endpoint]: "",
+  [WebhookProperty.webhook_auth_basic_credentials]: "",
+  [WebhookProperty.webhook_auth_jwt_secret]: "",
 };
