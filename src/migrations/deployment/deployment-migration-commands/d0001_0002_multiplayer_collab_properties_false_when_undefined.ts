0b7b4213c0c8f842819ce08055dc846469f75208
Ratan Khalsa <ratan.khalsa@gmetri.com>
2022-11-11 13:07:12 +0000

    migration to set collab properties false when undefined

---
+++ src/migrations/deployment/deployment-migration-commands/d0001_0002_multiplayer_collab_properties_false_when_undefined.ts
@@ -0,0 +1,32 @@
+import { RT, RecordNode, rtp, r } from "../../../r";
+import { IOrder } from "../../IOrder";
+
+
+/**
+ * Convert audio, screenshare, chat properties to false when they're undefined for older projects
+ */
+
+ class Migration implements IOrder {
+  execute(deploymentSettingsJson: unknown) {
+    const depSettingsJson = deploymentSettingsJson as unknown as RecordNode<RT.deployment>;
+    const deploymentF = r.record(depSettingsJson);
+
+    const propertiesArray = [
+      rtp.deployment.multiplayer_audio_enabled,
+      rtp.deployment.multiplayer_screenshare_enabled,
+      rtp.deployment.multiplayer_chat_enabled,
+    ];
+
+    propertiesArray.forEach(p => {
+      if(depSettingsJson.props[p] === undefined) {
+        depSettingsJson.props[p] = false;
+      }
+    });
+
+    deploymentF.set(rtp.deployment.deployment_version, 1);
+    return depSettingsJson;
+  }
+ }
+
+const migration = new Migration();
+export default migration;
