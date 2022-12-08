a8c739d1a7cc706c0608d6f73918fe07357e4f42
Ratan Khalsa <ratan.khalsa@gmetri.com>
2022-12-08 08:57:12 +0000

    removed redundant code

--- src/migrations/project/r-migration-commands/m145_146_update_custom_project_logo_source_to_project_logo_source.ts
+++ src/migrations/project/r-migration-commands/m145_146_update_custom_project_logo_source_to_project_logo_source.ts
@@ -8,13 +8,8 @@ class Migration implements IOrder {
   execute(projectJson: unknown) {
     const pJson = projectJson as RecordNode<RT.project>;
     const projectF = r.project(pJson);
-    const url = projectF.get(rtp.project.custom_project_logo_source);
-
-    if(url) {
-      projectF.set(rtp.project.project_logo_source, url);
-    }
-    
     projectF.changePropertyName("custom_project_logo_source", "project_logo_source");
+    
     projectF.set(rtp.project.version, 146);
   }
 }
