c2995890e7b193d2feefd1d80a0075917963f4cb
Ratan Khalsa <ratan.khalsa@gmetri.com>
2023-03-21 10:49:23 +0000

    addressing review comments

--- src/migrations/project/r-migration-commands/m148_149_migrate_popup_item_media_types.ts
+++ src/migrations/project/r-migration-commands/m148_149_migrate_popup_item_media_types.ts
@@ -1,4 +1,4 @@
-import { r, RecordNode, rtp, RT, fn } from "../../../r";
+import { r, RecordNode, rtp, RT, en, fn } from "../../../r";
 import { IOrder } from "../../IOrder";
 import { pathUtils } from "@gmetrixr/gdash";
 
@@ -6,33 +6,35 @@ import { pathUtils } from "@gmetrixr/gdash";
  * Migrating media type for popup items
  * Old Types: media, youtube, embed_code, sprite
  * New Types: image, video, audio, 3d_model, pdf, youtube, markdown, embed_code, sprite
+ * We check whether a file uploaded is an image, video, audio or 3d_model
+ * It CAN'T be a PDF as we currently don't support them
  */
 class Migration implements IOrder {
   execute(projectJson: unknown) {
     const pJson = projectJson as RecordNode<RT.project>;
     const projectF = r.record(pJson);
 
-    const allPopupElements = projectF.getAllDeepChildrenWithFilter(RT.element, el => el.props.element_type === "popup");
+    const allPopupElements = projectF.getAllDeepChildrenWithFilter(RT.element, el => el.props.element_type === en.ElementType.popup);
 
     for (const element of allPopupElements) {
       const elementF = r.element(element);
       const allItems = elementF.getRecords(RT.item);
 
       for(const item of allItems) {
-        const source = item.props.item_source as fn.Source;
-        let sourceUri = "";
+        const itemF = r.record(item);
+        const source = itemF.getValueOrDefault(rtp.item.item_source) as fn.Source;
+        let sourceType = source.type as pathUtils.FileType;
 
-        //* It's just the "media" type that will be split into image, video, audio, pdf or 3d_model
-        if(item.props.item_source_type === "media") {
-          if(typeof source === "object" && source.file_urls) {
-            sourceUri = source.file_urls ? (source.file_urls.gltf ?? source.file_urls.o): "";
-          }
+        //* Only in case of JSON corruption could this scenario occur
+        if(!sourceType) {
+          // get the file type from the file_urls
+          const url = source.file_urls?.glb || source.file_urls?.gltf || source.file_urls?.o || "";
+          sourceType = pathUtils.getFileType(url);
         }
 
-        const { getFileType, FileType } = pathUtils;
-        const fileType = getFileType(sourceUri);
+        const { FileType } = pathUtils;
 
-        switch(fileType) {
+        switch(sourceType) {
           case FileType.IMAGE: {
             item.props.item_source_type = "image";
             break;
@@ -48,10 +50,6 @@ class Migration implements IOrder {
           case FileType.THREED:
           case FileType.COMPRESSED: {
             item.props.item_source_type = "3d_model";
-            break;
-          }
-          case FileType.PDF: {
-            item.props.item_source_type = "pdf";
             break;
           }
         }
