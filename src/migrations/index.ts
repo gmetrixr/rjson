535d338a588416dd0166fc39f35c15a4f72720a2
Amit Rajput <amitrajput123.ar@gmail.com>
2023-05-11 14:55:20 +0530

    Separating out initial migrations and normal migrations, some migrations now require files to be injected before migrations can be applied

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,10 +1,11 @@
-import { createNewProject, migrateProjectRJson, migrationsForNewProject, getHighestProjectVersion, runHealthCheckMigrations, confirmNoCorruption } from "./project";
+import { createNewProject, migrateProjectRJson, migrationsForNewProject, getHighestProjectVersion, runHealthCheckMigrations, confirmNoCorruption, initialMigrateProjectRJson } from "./project";
 import { createNewDeployment, migrateDeployment, getHighestDeploymentVersion } from "./deployment";
 import * as gv from "./globalVariables";
 
 const migrations = {
   createNewProject, createNewDeployment,
-  migrateProjectRJson, migrationsForNewProject, getHighestProjectVersion, 
+  migrateProjectRJson, migrationsForNewProject, getHighestProjectVersion,
+  initialMigrateProjectRJson,
   runHealthCheckMigrations, confirmNoCorruption,
   migrateDeployment, getHighestDeploymentVersion,
 }
--- src/migrations/project/migrationLists.ts
+++ src/migrations/project/migrationLists.ts
@@ -52,6 +52,7 @@ import m148_149 from "./r-migration-commands/m148_149_migrate_popup_item_media_t
 import m149_150 from "./r-migration-commands/m149_150_add_legacy_color_management_flag";
 import m150_151 from "./r-migration-commands/m150_151_add_player_count_var_predefined_variable";
 import m151_152 from "./r-migration-commands/m151_152_changing_defaults_avatar_system";
+import m152_153 from "./r-migration-commands/m152_153_rerun_migrate_popoup_item";
 
 import create_first_scene from "./newproject-migration-commands/create_first_scene";
 import create_predefined_vars from "./newproject-migration-commands/create_predefined_vars";
@@ -112,6 +113,7 @@ export const rMigrationTree: {[key: number]: IOrder} = {
   [149]: m149_150,
   [150]: m150_151,
   [151]: m151_152,
+  [152]: m152_153,
 };
 
 export const getHighestProjectVersion = (): number => {
