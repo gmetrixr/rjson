3f5011b742ce7e02ddacc7a1cb0301871db40bfe
Siddharth <siddharth.pokharna@gmetri.com>
2023-06-09 11:21:38 +0530

    [add] character scale migration

---
+++ src/migrations/project/r-migration-commands/m153_154_changing_default_for_character_scale.ts
@@ -0,0 +1,23 @@
+import { RecordNode, r, rtp, RT, en } from "../../../r";
+import { IOrder } from "../../IOrder";
+
+class Migration implements IOrder {
+  execute(projectJson: unknown) {
+    const pJson = projectJson as RecordNode<RT.project>;
+    const projectF = r.project(pJson);
+
+    const allCharacterElements = projectF.getAllDeepChildrenWithFilter(RT.element, el => el.props.element_type === en.ElementType.character);
+
+    for (const el of allCharacterElements) {
+      if (el.props.scale === undefined) {
+        const elementF = r.element(el);
+        elementF.set(rtp.element.scale, 1);
+      }
+    }
+    
+    projectF.set(rtp.project.version, 153);
+  }
+}
+
+const migration = new Migration();
+export default migration;
